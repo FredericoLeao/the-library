401c208c8bba721902d9c68691e1d321287c2fc4
Mario F. Leão <fredericoleaob@gmail.com>
2022-12-26 13:46:37 -0300

    Add Maska to VueJS

--- thelibraryjs/src/main.js
+++ thelibraryjs/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { vMaska } from 'maska'
 import "bootstrap/dist/css/bootstrap.min.css"
 
 
 const app = createApp(App)
+app.directive('maska', vMaska)
 app.use(router)
 app.mount('#app')
