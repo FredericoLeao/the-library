0005a81f44556b787bee3580afac65ac3f7f5e72
Mario F. Leão <fredericoleaob@gmail.com>
2022-12-27 00:43:59 -0300

    wip bookForm, saving authors and categories correctly

--- thelibraryjs/src/views/Books/BookForm.vue
+++ thelibraryjs/src/views/Books/BookForm.vue
@@ -1,86 +1,95 @@
 <template>
-  <modal-dialog
-    title="Add Book"
-    ref="bookFormModal"
-    btn-ok-text="Salvar"
-    @ok="okButton()">
-    <div>
-      <div class="row">
-        <div class="col-12">
-          <label for="book-title">ISBN:</label>
-          <input type="text" id="book-isbn" class="form-control" maxlength="20" v-model="bookInstance.isbn">
+  <div>
+    <modal-dialog
+      title="Add Book"
+      ref="bookFormModal"
+      btn-ok-text="Salvar"
+      @ok="okButton()">
+      <div>
+        <div class="row">
+          <div class="col-12">
+            <label for="book-title">ISBN:</label>
+            <input type="text" id="book-isbn" class="form-control" maxlength="20" v-model="bookInstance.isbn">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <label for="book-title">Title:</label>
+            <input type="text" id="book-title" class="form-control" v-model="bookInstance.title">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <label for="book-title">Description:</label>
+            <textarea id="book-description" class="form-control" v-model="bookInstance.description"></textarea>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <label for="book-authors">Authors:</label>
+            <v-select
+              class="form-control shadow"
+              multiple
+              id="author-select"
+              @search="searchAuthor"
+              :options="authorOptions"
+              v-model="selectedAuthors">
+              <template #option="{ label }">
+                {{ label }}
+              </template>
+              <template #selected-option="{ label }">
+                {{ label }}
+              </template>
+              <template #no-options="">
+                <small>
+                  Type your search to find some author...
+                </small>
+              </template>
+            </v-select>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <label for="book-categories">Categories:</label>
+            <vSelect
+              multiple
+              id="category-select"
+              @search="searchCategory"
+              :options="categoryOptions"
+              v-model="selectedCategories"
+              class="form-control shadow">
+              <template #option="{ label }">
+                {{ label }}
+              </template>
+              <template #selected-option="{ label }">
+                {{ label }}
+              </template>
+              <template #no-options="">
+                <small>
+                  Type your search to find some category...
+                </small>
+              </template>
+            </vSelect>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <label for="book-cover">Capa:</label>
+            <input type="file" id="book-cover" class="form-control">
+          </div>
         </div>
       </div>
-      <div class="row">
-        <div class="col-12">
-          <label for="book-title">Title:</label>
-          <input type="text" id="book-title" class="form-control" v-model="bookInstance.title">
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-12">
-          <label for="book-title">Description:</label>
-          <textarea id="book-description" class="form-control" v-model="bookInstance.description"></textarea>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-12">
-          <label for="book-authors">Authors:</label>
-          <vSelect
-            multiple
-            id="author-select"
-            @search="searchAuthor"
-            :options="authorOptions"
-            v-model="bookInstance.authors"
-            label="name"
-            class="form-control shadow">
-            <template #option="{ name }">
-              {{ name }}
-            </template>
-            <template #selected-option="{ name }">
-              {{ name }}
-            </template>
-            <template #no-options="">
-              <small>
-                Type your search to find some author...
-              </small>
-            </template>
-          </vSelect>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-12">
-          <label for="book-categories">Categories:</label>
-          <vSelect
-            multiple
-            id="category-select"
-            @search="searchCategory"
-            :options="categoryOptions"
-            v-model="bookInstance.categories"
-            label="name"
-            class="form-control shadow">
-            <template #option="{ name }">
-              {{ name }}
-            </template>
-            <template #selected-option="{ name }">
-              {{ name }}
-            </template>
-            <template #no-options="">
-              <small>
-                Type your search to find some category...
-              </small>
-            </template>
-          </vSelect>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-12">
-          <label for="book-cover">Capa:</label>
-          <input type="file" id="book-cover" class="form-control">
-        </div>
-      </div>
-    </div>
-  </modal-dialog>
+    </modal-dialog>
+    <modal-dialog
+      title="Book Form"
+      ref="bookFormResponseModal"
+      btn-ok-text="Fechar"
+      :btn-cancel-visible="false"
+      @ok="$refs.bookFormResponseModal.hide()">
+      <div v-if="responseStatus===201">Book's data was saved</div>
+      <div v-else-if="responseStatus && responseStatus!==201">Error... this system may need a doctor</div>
+    </modal-dialog>
+  </div>
 </template>
 
 <script>
@@ -91,6 +100,7 @@ import ModalDialog from '@/components/Modal.vue'
 
 export default {
   name: 'BookForm',
+  emits: ['update-books'],
   components: {
     vSelect,
     ModalDialog
@@ -106,8 +116,11 @@ export default {
         categories: [],
         cover: ''
       },
+      selectedAuthors: [],
+      selectedCategories: [],
       authorOptions: [],
-      categoryOptions: []
+      categoryOptions: [],
+      responseStatus: null
     }
   },
 
@@ -118,7 +131,7 @@ export default {
         axios.get('http://localhost/api/categories/lookup/', { params: { lookup_string: search }})
         .then(response => {
           if (response.status === 200) {
-            this.categoryOptions = response.data
+            this.categoryOptions = response.data.map(category => { return { value: category.id, label: category.name }})
           }
         })
       }
@@ -130,13 +143,15 @@ export default {
         axios.get('http://localhost/api/authors/lookup/', { params: { lookup_string: search }})
         .then(response => {
           if (response.status === 200) {
-            this.authorOptions = response.data
+            this.authorOptions = response.data.map(author => { return { value: author.id, label: author.name }})
           }
         })
       }
     },
 
     saveInstance () {
+      this.bookInstance.authors = this.selectedAuthors.map(author => author.value)
+      this.bookInstance.categories = this.selectedCategories.map(category => category.value)
       if (!this.bookInstance.id) {
         axios.post('http://localhost/api/books/', this.bookInstance).then((response) => {
           this.responseStatus = response.status
@@ -164,13 +179,14 @@ export default {
       this.$refs.bookFormModal.show()
     },
 
-    //showFormResponse () {
-    //  this.$refs.authorFormResponseModal.show()
-    //},
-//
-    //hideFormResponse () {
-    //  this.$refs.authorFormResponseModal.hide()
-    //}
+    showFormResponse () {
+      this.$refs.bookFormResponseModal.show()
+    },
+
+    hideFormResponse () {
+      this.$refs.bookFormResponseModal.hide()
+    },
+
     okButton () {
       this.saveInstance()
     }
