160097a532938309584068b27be5ce7f27fe9b7b
Mario F. Leão <fredericoleaob@gmail.com>
2022-12-27 02:02:04 -0300

    add bootstrap-icons to vuejs

--- thelibraryjs/src/main.js
+++ thelibraryjs/src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { vMaska } from 'maska'
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 
 const app = createApp(App)
